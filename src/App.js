61357c43ff66f2b754f35733a84f87f1fdf0419f
Prabhjot Singh <prabhjotsingh394@gmail.com>
2021-10-24 11:24:56 +0530

    rotes, switch and exact

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
 import MainHeader from './components/MainHeader';
+import ProductDetails from './pages/ProductDetails';
 
 function App() {
   return (
     <div>
       <MainHeader />
       <main>
-        <Route path="/welcome">
-          <Welcome />
-        </Route>
+        <Switch>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
 
-        <Route path="/products">
-          <Products />
-        </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+
+          <Route path="/products/:productId">
+            <ProductDetails />
+          </Route>
+        </Switch>
       </main>
     </div>
   );
